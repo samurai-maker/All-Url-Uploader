903134d24d5d986c2ec9bf96eff01b91db0728e6
samurai-maker <84018991+samurai-maker@users.noreply.github.com>
2022-03-22 06:26:02 -0700

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -46,8 +46,8 @@ async def start(bot, message):
     reply_markup=InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton("Comment", url="https://t.me/TMWAD/18"),
-                    InlineKeyboardButton("🤖 Updates", url="https://t.me/TMWAD")
+                    InlineKeyboardButton("WWM MOVIE GROUP", url="https://t.me/WORLD_WIDE_MOVIES"),
+                    InlineKeyboardButton("MALAYALAM CHATTING", url="https://t.me/MALAYALAM_CHATTING")
                 ]
             ]
         ),
